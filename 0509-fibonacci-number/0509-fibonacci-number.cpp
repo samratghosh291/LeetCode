4ca9a219f928fe1cdba9d3e62a884cd1861e3a42
Samrat Ghosh <101434286+samratghosh291@users.noreply.github.com>
2022-10-23 13:14:29 +0530

    Time: 15 ms (43.18%), Space: 5.9 MB (38.12%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,7 +2,12 @@ class Solution {
 public:
     int fib(int n) {
         
-        return n <= 1  ? n : fib(n - 1) + fib(n - 2);
+        if(n<=1){
+            return n;
+        }
+        else{
+            return fib(n-1)+fib(n-2);
+        }
         
     }
 };

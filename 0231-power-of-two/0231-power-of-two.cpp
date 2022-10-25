03b4b38e761f125facf3606cd17e37559dc7d661
Samrat Ghosh <101434286+samratghosh291@users.noreply.github.com>
2022-10-25 08:31:37 +0530

    Time: 3 ms (64.62%), Space: 5.9 MB (71.39%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         
+        
+        //all the power of 2 should be positive
         if(n<0 || n==0){
             return false;
         }

350adea46de67799ffa5be84d7f506a3e41c11e3
Samrat Ghosh <101434286+samratghosh291@users.noreply.github.com>
2023-07-15 16:22:48 +0530

    Time: 21 ms (27.80%), Space: 13.1 MB (99.38%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,28 +3,34 @@ public:
     void setZeroes(vector<vector<int>>& arr) {
         int r = arr.size();
         int c = arr[0].size();
-        
-        // Create vectors to track rows and columns with zeros
-        vector<int> v_row(r);  // Track rows with zeros
-        vector<int> v_col(c);  // Track columns with zeros
-        
+
+        bool flag = false; // Flag to track if the first column has a zero
+
         // Step 1: Mark rows and columns with zeros
         for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
+            if (arr[i][0] == 0) {
+                flag = true; // Set the flag if the first column has a zero
+            }
+
+            for (int j = 1; j < c; j++) {
                 if (arr[i][j] == 0) {
-                    v_row[i] = 1;  // Mark row i as having a zero
-                    v_col[j] = 1;  // Mark column j as having a zero
+                    arr[i][0] = 0; // Mark the corresponding row with a zero
+                    arr[0][j] = 0; // Mark the corresponding column with a zero
                 }
             }
         }
-        
+
         // Step 2: Set rows and columns with zeros to zeros
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (v_row[i] == 1 || v_col[j] == 1) {
-                    arr[i][j] = 0;  // Set element at (i, j) to zero
+        for (int i = r - 1; i >= 0; i--) {
+            for (int j = c - 1; j >= 1; j--) {
+                if (arr[i][0] == 0 || arr[0][j] == 0) {
+                    arr[i][j] = 0; // Set the element to zero if its row or column is marked with a zero
                 }
             }
+
+            if (flag) {
+                arr[i][0] = 0; // Set the first column to zero if the flag is set
+            }
         }
-    }      
+    }
 };

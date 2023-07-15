c78afef98238f455dc770e65225c4afe22b9a35b
Samrat Ghosh <101434286+samratghosh291@users.noreply.github.com>
2023-07-15 13:10:29 +0530

    Time: 21 ms (27.80%), Space: 13.4 MB (29.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>&arr) {
-        int r=arr.size();
-        int c=arr[0].size();
-        vector<int>v_row(r);
-        vector<int>v_col(c);
+    void setZeroes(vector<vector<int>>& arr) {
+        int r = arr.size();
+        int c = arr[0].size();
         
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                
-                if(arr[i][j]==0){
-                    v_row[i]=1;
-                    v_col[j]=1;
+        // Create vectors to track rows and columns with zeros
+        vector<int> v_row(r);  // Track rows with zeros
+        vector<int> v_col(c);  // Track columns with zeros
+        
+        // Step 1: Mark rows and columns with zeros
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (arr[i][j] == 0) {
+                    v_row[i] = 1;  // Mark row i as having a zero
+                    v_col[j] = 1;  // Mark column j as having a zero
                 }
             }
         }
         
-        
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(v_row[i]==1 or v_col[j]==1){
-                    arr[i][j]=0;
+        // Step 2: Set rows and columns with zeros to zeros
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (v_row[i] == 1 || v_col[j] == 1) {
+                    arr[i][j] = 0;  // Set element at (i, j) to zero
                 }
             }
-    }
-        
+        }
     }      
 };

90d93d7af08aff69cbcb3cf2d64d3d7b58218293
Samrat Ghosh <101434286+samratghosh291@users.noreply.github.com>
2023-07-15 09:10:55 +0530

    Time: 29 ms (85.35%), Space: 17.5 MB (98.62%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -1,41 +1,64 @@
 class Solution {
 public:
-    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        int n=mat.size(),m=mat[0].size();
-        vector<int>ans(n*m);
-        int i=0; //points to where we need to put element in array
-        int r=0,c=0;//starting position
+    vector<int> findDiagonalOrder(vector<vector<int>>& arr) {
+        int row=arr.size();
+        int col=arr[0].size();
+        vector<int>v(row*col); // to be stored array
+        int i=0;
+        int j=0;
+        int idx=0;  //index of stored array
         bool up=true;
-        while(r<n && c<m){
-          //if diagonal is going up
-          if(up){
-              while(r>0 && c<m-1){
-                  ans[i++]=mat[r][c];
-                  r--;
-                  c++;
-              }
-              ans[i++]=mat[r][c];//to put uppermost right element in ans
-              if(c==m-1){
-                  r++;
-              }else{
-                  c++;
-              }
-          }
-          //or down
-          else{
-              while(c>0 && r<n-1){
-                  ans[i++]=mat[r][c];
-                  r++;c--;
-              }
-              ans[i++]=mat[r][c];//to put value 4 in it
-              if(r==n-1){
-                  c++;
-              }else{
-                  r++;
-              }
-          }
-          up=!up;
-        }        
-        return ans;
+        
+        while(i<row and j<col){
+            
+            ///for upward direction
+            if(up){
+                
+                while(i>0 and j<(col-1)){
+                    
+                    v[idx++]=arr[i][j];
+                    i--;
+                    j++;
+                    
+                }
+                
+                v[idx++]=arr[i][j];    //stored the terminal element
+                
+                if(j==(col-1)){    // when j reached point at terminal column element
+                    i++;
+                }
+                else{
+                    j++;
+                }   
+            }
+            
+            // for downward direction 
+            
+            else{
+                
+             while(i<(row-1) and j>0) {
+                 
+               v[idx++]=arr[i][j];
+                 i++;
+                 j--;
+                        
+             }
+                
+             v[idx++]=arr[i][j];  //stored the terminal element
+                
+                if(i==(row-1)){    // when j reached point at terminal row element
+                    j++;
+                }
+                else{
+                    i++;
+                }
+                
+            }
+            
+            up=!up;   //toggle the up value
+        }
+        
+        return v;
+        
     }
 };

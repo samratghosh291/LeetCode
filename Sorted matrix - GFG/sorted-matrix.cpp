05f90632753b0b69c4fe3bd94c647af91224a5ea
Samrat Ghosh <101434286+samratghosh291@users.noreply.github.com>
2023-07-20 21:11:17 +0530

    Added solution - LeetHub

---
+++ Sorted matrix - GFG/sorted-matrix.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> sortedMatrix(int n, vector<vector<int>> mat) {
+        // code here
+        vector<int>v;
+        
+        
+        //copy the whole matrics in v vector linearly.
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                v.push_back(mat[i][j]);
+            }
+        }
+        
+        // sort the v vector.
+        sort(v.begin(),v.end());
+        
+        
+        // insert the elements of v vector inside mat matrics
+        int idx=0;
+         for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                mat[i][j]=v[idx++];
+            }
+        }
+        return mat;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> v(N, vector<int>(N));
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) cin >> v[i][j];
+        Solution ob;
+        v = ob.sortedMatrix(N, v);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) cout << v[i][j] << " ";
+            cout << "\n";
+        }
+    }
+}
+// } Driver Code Ends

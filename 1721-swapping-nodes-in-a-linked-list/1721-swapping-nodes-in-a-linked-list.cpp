b56494c8269af3c2ea5cb946d5dd38757da9f75e
Samrat Ghosh <101434286+samratghosh291@users.noreply.github.com>
2023-08-11 00:27:49 +0530

    Time: 539 ms (82.30%), Space: 180.1 MB (28.06%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -1,3 +1,35 @@
+// Approach using three loop
+// TC:0(N)
+// SC:O(1)
+//  //count the loop
+//         int count=0;
+        
+//         while(curr){
+//             count++;
+//             curr=curr->next;   
+//         }
+        
+//         //position left pointer at k position from begining
+        
+//         for(int i=1;i<k;i++){
+//             left=left->next;
+//         }
+        
+//         //position left pointer at k position from end
+        
+//         for(int i=1;i<(count+1-k);i++){
+//             right=right->next;
+//         }
+        
+        
+//         //swip their values
+//         int temp=left->val;
+//         left->val=right->val;
+//         right->val=temp;
+        
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -19,28 +51,25 @@ public:
         ListNode* left=head;
         ListNode* right=head;
         ListNode* curr=head;
+        int count=1;
+        while(curr!=NULL) // in a single traversal we will find kth and (n-k)th node
+        {
+            if(count<k)
+                    {
+// first moving left pointer so that right pointer lags exactly k nodes behind left pointer and left pointer be fixed at kth node
+                        left=left->next;
+                    }
+            if(count>k) 
+// when count exceedes k it means left pointer has location of nth node .
+                    {
+                        right=right->next;
+                    }
+                        curr=curr->next;
+                        count++;
+                    }
+       
         
-        
-        //count the loop
-        int count=0;
-        
-        while(curr){
-            count++;
-            curr=curr->next;   
-        }
-        
-        //position left pointer at k position from begining
-        
-        for(int i=1;i<k;i++){
-            left=left->next;
-        }
-        
-        //position left pointer at k position from end
-        
-        for(int i=1;i<(count+1-k);i++){
-            right=right->next;
-        }
-        
+        //swip their values
         int temp=left->val;
         left->val=right->val;
         right->val=temp;

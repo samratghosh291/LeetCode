6dab84373b75bb2718d667012d5e270f7616f6cf
Samrat Ghosh <101434286+samratghosh291@users.noreply.github.com>
2023-08-20 03:39:38 +0530

    Added solution - LeetHub

---
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	int count(int arr[], int n, int x) {
+	    // code here
+	    
+	    
+	    //decleat the map
+	    map<int,int>mp;
+	    
+	    
+	    //store the frequency in map
+	    for(int i=0;i<n;i++){
+	        
+	        mp[arr[i]]++;
+	    }
+	    
+	    //find the desired element and return
+	    if(mp.find(x)!=mp.end()){
+	        
+	        return mp[x];
+	    }
+	     return 0;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.count(arr, n, x);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends

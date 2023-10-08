07661aae39b98bb071cf1278c4954ec7073897ba
Samrat Ghosh <101434286+samratghosh291@users.noreply.github.com>
2023-10-09 00:49:11 +0530

    Added solution - LeetHub

--- Perfect Numbers - GFG/perfect-numbers.cpp
+++ Perfect Numbers - GFG/perfect-numbers.cpp
@@ -8,16 +8,25 @@ class Solution {
     int isPerfectNumber(long long n) {
         // code here
         
-        long long ans=0;
+        long long ans=0;  //initialize the ans as 0
         
-        for (int i=1;i<=sqrt(n);i++){
+        for (int i=1;i<=sqrt(n);i++){   //run the loop upto sqrt(n)
             
             if(n%i==0){
-                ans+=i;
-                ans+=(n/i);  
+                ans+=i;       //add the fatctor
+                
+                if(i!=(n%i)){   //check wheathe the factor is not sqrt(n)
+                
+                    ans+=(n/i);   //add anothe factor
+                }
             }
         }
         
+        // now ans is the sum of all unique factor of ans 
+        
+        
+        
+        // check wheathe rthe given no. is not 1 or perect 
         if((ans-n)==n and n!=1){
             return 1;
         }
